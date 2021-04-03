2bbcb90b9c5a7b7c3eda64f98ad2a6e53ffa3280
mtkane-ki <michael@kaneinformatics.io>
2021-04-03 11:30:49 -0400

    commentary

--- fileActions.js
+++ fileActions.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
-const fsSync = require("fs").promises;
-const csvParse = require("csv-parse/lib/sync");
+const fs = require("fs"); // library which is responsible for file system actions
+const fsSync = require("fs").promises; //library which lets us return a promise on a file system action
+const csvParse = require("csv-parse/lib/sync"); //library which lets us synchronously parse csv file data
 
 function LoadDesiredStates() {
   const fileRaw = fs.readFileSync("./desiredStateStore.json", (err, data) => {
@@ -9,7 +9,7 @@ function LoadDesiredStates() {
     }
   });
   return JSON.parse(fileRaw);
-}
+} //pull in the desired state JSON file from disk
 
 function LoadCurrentCDCFile() {
   const fileRaw = fs.readFileSync("./currentQueryData.json", (err, data) => {
@@ -18,20 +18,20 @@ function LoadCurrentCDCFile() {
     }
   });
   return JSON.parse(fileRaw);
-}
+} //pull in the current bot data from disk
 
 async function LoadDownloadedCDCFile(downloadPath) {
-  const filePath = `${downloadPath}covid19_vaccinations_in_the_united_states.csv`;
+  const filePath = `${downloadPath}covid19_vaccinations_in_the_united_states.csv`; //create path variable
   const fileRaw = await fsSync.readFile(filePath, (err, data) => {
     if (err) {
       throw err;
     }
-  });
+  });// pull in csv data synchronously
 
-  const csvData = csvParse(fileRaw, { columns: true, from_line: 4 });
-  DeleteFile(filePath);
+  const csvData = csvParse(fileRaw, { columns: true, from_line: 4 }); //parse data into js object
+  DeleteFile(filePath); //delete temp file
   return csvData;
-}
+} //pull in the csv file, parse it, and delete it
 
 function LoadPreviousCDCFile() {
   const fileRaw = fs.readFileSync("./previousQueryData.json", (err, data) => {
@@ -40,7 +40,7 @@ function LoadPreviousCDCFile() {
     }
   });
   return JSON.parse(fileRaw);
-}
+} //pull in historical bot data
 
 function DeleteFile(filePath) {
   fs.unlink(filePath, (err) => {
@@ -49,18 +49,18 @@ function DeleteFile(filePath) {
       return;
     }
   });
-}
+} //delete a given file from disk
 
 function writeCDCFiles(currentData, oldData) {
   fs.writeFileSync(
     "./previousQueryData.json",
-    JSON.stringify(oldData, null, 2)
+    JSON.stringify(oldData, null, 2) //write to file in pretty print
   );
   fs.writeFileSync(
     "./currentQueryData.json",
-    JSON.stringify(currentData, null, 2)
+    JSON.stringify(currentData, null, 2) //write to file in pretty print
   );
-}
+} //update bot data files
 
 module.exports = {
   LoadDesiredStates,
