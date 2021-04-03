672e4e67c2c23b4cf5f72b29f499fc8465b97e30
mtkane-ki <michael@kaneinformatics.io>
2021-04-03 10:37:10 -0400

    remove state level census objects

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.on("message", async (msg) => {
 
   if (command === "vaxtrack") {
     const stateList = fileActions.LoadDesiredStates();
-    const popData = await censusQuery.populationQuery(stateList.Census);
+    const popData = await censusQuery.populationQuery();
     const data = await cdcQuery.getCuratedCDCData(
       (await fileActions.LoadDesiredStates()).CDC,
       downloadPath
